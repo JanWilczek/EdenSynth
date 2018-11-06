6852e40e4dc3bd5d919308094a4bfbfb7edd2b51
Jan Wilczek <janekw@vp.pl>
2018-11-06 19:16:20 +0100

    Changed: {SharedCode} File hierarchy.

--- EdenSynth/SharedCode/source/PluginEditor.cpp
+++ EdenSynth/SharedCode/source/PluginEditor.cpp
@@ -9,7 +9,7 @@ EdenSynthAudioProcessorEditor::EdenSynthAudioProcessorEditor(EdenSynthAudioProce
 {
 	// Make sure that before the constructor has finished, you've set the
 	// editor's size to whatever you need it to be.
-	setSize(400, 300);
+	setSize(1000, 500);
 
 	_title.setFont(_titleFont);
 	addAndMakeVisible(&_title);
@@ -37,10 +37,6 @@ void EdenSynthAudioProcessorEditor::paint(Graphics& g)
 {
 	// (Our component is opaque, so we must completely fill the background with a solid colour)
 	g.fillAll(getLookAndFeel().findColour(ResizableWindow::backgroundColourId));
-
-	g.setColour(Colours::white);
-	g.setFont(15.0f);
-	g.drawFittedText("Hello World!", getLocalBounds(), Justification::centred, 1);
 }
 
 void EdenSynthAudioProcessorEditor::resized()
