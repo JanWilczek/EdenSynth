6053f4fc156ac63a9b93a99eddcb024536504651
Jan Wilczek <janekw@vp.pl>
2018-12-03 23:16:14 +0100

    Fixed: {SharedCode} Int to float conversion.

--- EdenSynth/SharedCode/source/WaveshapingCanvas.cpp
+++ EdenSynth/SharedCode/source/WaveshapingCanvas.cpp
@@ -12,7 +12,7 @@ void WaveshapingCanvas::resized()
 
 void WaveshapingCanvas::paint(Graphics& g)
 {
-	g.fillAll(getLookAndFeel().findColour(ResizableWindow::backgroundColourId));
+	g.fillAll(getLookAndFeel().findColour(ResizableWindow::backgroundColourId).brighter(0.1f));
 
 	drawPlotAxes(g);
 
@@ -25,18 +25,18 @@ void WaveshapingCanvas::paint(Graphics& g)
 void WaveshapingCanvas::drawPlotAxes(Graphics& g)
 {
 	g.setColour(Colours::lightgrey);
-	g.drawHorizontalLine(getHeight() / 2, 0.f, getWidth());
-	g.drawVerticalLine(getWidth() / 2, 0.f, getHeight());
+	g.drawHorizontalLine(getHeight() / 2, 0.f, static_cast<float>(getWidth()));
+	g.drawVerticalLine(getWidth() / 2, 0.f, static_cast<float>(getHeight()));
 
-	Line<float> leftVerticalPlotLine(getWidth() / 4, 0.f, getWidth() / 4, getHeight());
+	Line<float> leftVerticalPlotLine(getWidth() / 4.f, 0.f, getWidth() / 4.f, static_cast<float>(getHeight()));
 	constexpr float dashes[] = { 7.f, 5.f };
 	g.drawDashedLine(leftVerticalPlotLine, dashes, 2, 0.5f);
-	leftVerticalPlotLine.applyTransform(AffineTransform(1.f, 0.f, 0.f, 0.f, 1.f, 0.f).translated(getWidth() / 2, 0.f));
+	leftVerticalPlotLine.applyTransform(AffineTransform(1.f, 0.f, 0.f, 0.f, 1.f, 0.f).translated(getWidth() / 2.f, 0.f));
 	g.drawDashedLine(leftVerticalPlotLine, dashes, 2, 0.5f);
 
-	Line<float> upperHorizontalPlotLine(0.f, getHeight() / 4, getWidth(), getHeight() / 4);
+	Line<float> upperHorizontalPlotLine(0.f, getHeight() / 4.f, static_cast<float>(getWidth()), getHeight() / 4.f);
 	g.drawDashedLine(upperHorizontalPlotLine, dashes, 2, 0.5f);
-	upperHorizontalPlotLine.applyTransform(AffineTransform(1.f, 0.f, 0.f, 0.f, 1.f, 0.f).translated(0.f, getHeight() / 2));
+	upperHorizontalPlotLine.applyTransform(AffineTransform(1.f, 0.f, 0.f, 0.f, 1.f, 0.f).translated(0.f, getHeight() / 2.f));
 	g.drawDashedLine(upperHorizontalPlotLine, dashes, 2, 0.5f);
 }
 
@@ -50,7 +50,7 @@ void WaveshapingCanvas::drawTransferFunction(Graphics& g)
 	const auto nb_points = static_cast<int>(_points.size());
 	for (auto i = 1; i < nb_points; ++i)
 	{
-		path.lineTo(i, getHeight() - (scale + scale * _points[i]));
+		path.lineTo(static_cast<float>(i), getHeight() - (scale + scale * _points[i]));
 	}
 
 	g.setColour(Colours::aqua);
