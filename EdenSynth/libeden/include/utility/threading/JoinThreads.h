5853bfb399171c4c5aa7ebc98e3c559c98808746
Jan Wilczek <janekw@vp.pl>
2019-03-03 13:39:39 +0100

    Added: {libeden} JoinThreads implementation.

--- EdenSynth/libeden/include/utility/threading/JoinThreads.h
+++ EdenSynth/libeden/include/utility/threading/JoinThreads.h
@@ -3,11 +3,23 @@
 /// \author Jan Wilczek
 /// \date 03.03.2019
 /// 
+#include <vector>
+#include <thread>
 
 namespace eden::utility::threading
 {
+	/// <summary>
+	/// Simple utility that ensures all threads from a vector are joined
+	/// when the object is destroyed. Implementation given by Anthony Williams 
+	/// in the book "C++ Concurrency in Action".
+	/// </summary>
 	class JoinThreads
 	{
-		
+	public:
+		explicit JoinThreads(std::vector<std::thread>& threads);
+		~JoinThreads();
+
+	private:
+		std::vector<std::thread>& _threads;
 	};
 }
