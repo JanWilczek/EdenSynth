c927161bdecc5a533b48f78d25be9f8016f46d90
Jan Wilczek <janekw@vp.pl>
2020-04-03 11:34:51 +0200

    Added: Inefficient but working correlation implementation.

--- EdenSynth/libeden_test/utility_test/dsp_test/CorrelationTest.cpp
+++ EdenSynth/libeden_test/utility_test/dsp_test/CorrelationTest.cpp
@@ -13,5 +13,15 @@ namespace libeden_test
 		const std::vector<float> dirac2{ 0.f, 0.f, 1.f, 0.f };
 
 		const auto result = correlation<CorrelationMode::Full>(dirac1, dirac2);
+
+		ASSERT_EQ(result.size(), 7u);
+		ASSERT_NEAR(result[2], 1.f / 7.f, 1e-3);
+		for (auto i = 0u; i < result.size(); ++i)
+		{
+			if (i != 2u)
+			{
+				ASSERT_EQ(result[i], 0.f);
+			}
+		}
 	}
 }
