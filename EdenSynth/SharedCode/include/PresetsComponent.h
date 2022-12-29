fbc5683eeeeed79d057d4c386afefffea8cb51e6
Jan Wilczek <janekw@vp.pl>
2022-12-29 17:02:37 +0100

    Finished the presets component GUI.

--- EdenSynth/SharedCode/include/PresetsComponent.h
+++ EdenSynth/SharedCode/include/PresetsComponent.h
@@ -1,15 +1,18 @@
 #pragma once
 
-#include <JuceHeader.h>
+#include "JuceHeader.h"
 
 class PresetsComponent : public juce::Component {
 public:
   PresetsComponent();
-  ~PresetsComponent() override;
 
   void paint(juce::Graphics&) override;
   void resized() override;
 
 private:
+  Label _presetLabel{"presetLabel", "Preset: "};
+  ComboBox _preset;
+  TextButton _savePresetButton;
+
   JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PresetsComponent)
 };
--- EdenSynth/SharedCode/source/PresetsComponent.cpp
+++ EdenSynth/SharedCode/source/PresetsComponent.cpp
@@ -1,20 +1,38 @@
 #include "PresetsComponent.h"
-#include "JuceHeader.h"
 
-PresetsComponent::PresetsComponent() {}
-
-PresetsComponent::~PresetsComponent() {}
+PresetsComponent::PresetsComponent() {
+  _presetLabel.setJustificationType(Justification::right);
+  addAndMakeVisible(_presetLabel);
+  addAndMakeVisible(_preset);
+  _savePresetButton.setButtonText("Save preset");
+  addAndMakeVisible(_savePresetButton);
+}
 
 void PresetsComponent::paint(juce::Graphics& g) {
   g.setColour(
       getLookAndFeel().findColour(Slider::ColourIds::textBoxOutlineColourId));
   g.drawRect(getBounds(), 5);
-
-  // placeholder text
-  g.setColour(juce::Colours::white);
-  g.setFont(14.0f);
-  g.drawText("PresetsComponent", getLocalBounds(), juce::Justification::centred,
-             true);
 }
 
-void PresetsComponent::resized() {}
+void PresetsComponent::resized() {
+  const auto PRESET_LABEL_WIDTH = getWidth() / 8;
+  const auto PRESET_BOX_WIDTH = getWidth() / 2;
+  const auto PRESET_BOX_HEIGHT = getHeight() / 2;
+  const auto PRESET_BOX_X = (getWidth() - PRESET_BOX_WIDTH) / 2;
+  const auto PRESET_BOX_Y = (getHeight() - PRESET_BOX_HEIGHT) / 2;
+  const auto SAVE_PRESET_BUTTON_WIDTH = getWidth() / 8;
+  const auto SAVE_PRESET_BUTTON_HEIGHT = PRESET_BOX_HEIGHT;
+  const auto SAVE_PRESET_BUTTON_Y = PRESET_BOX_Y;
+  constexpr auto COMPONENT_SPACING = 10;
+  const auto PRESET_LABEL_X =
+      PRESET_BOX_X - COMPONENT_SPACING - PRESET_LABEL_WIDTH;
+
+  _presetLabel.setBounds(PRESET_LABEL_X, 0, PRESET_LABEL_WIDTH, getHeight());
+  _preset.setBounds(PRESET_BOX_X, PRESET_BOX_Y, PRESET_BOX_WIDTH,
+                    PRESET_BOX_HEIGHT);
+
+  const auto SAVE_PRESET_BUTTON_X = _preset.getRight() + COMPONENT_SPACING;
+  _savePresetButton.setBounds(SAVE_PRESET_BUTTON_X, SAVE_PRESET_BUTTON_Y,
+                              SAVE_PRESET_BUTTON_WIDTH,
+                              SAVE_PRESET_BUTTON_HEIGHT);
+}
