fc6247c017bb244a760e588bd31f2f796d698842
Jan Wilczek <janekw@vp.pl>
2022-12-29 16:31:03 +0100

    SharedCode: Removed magical constants in PluginEditor.

--- EdenSynth/SharedCode/source/PluginEditor.cpp
+++ EdenSynth/SharedCode/source/PluginEditor.cpp
@@ -7,6 +7,15 @@ constexpr auto PRESETS_COMPONENT_HEIGHT = 50;
 constexpr auto SYNTHESIZER_CONTROLS_COMPONENTS_HEIGHT = 500;
 constexpr auto EDITOR_HEIGHT =
     PRESETS_COMPONENT_HEIGHT + SYNTHESIZER_CONTROLS_COMPONENTS_HEIGHT;
+
+constexpr auto GENERAL_SETTINGS_COMPONENT_WIDTH = 120;
+constexpr auto GENERATOR_COMPONENT_WIDTH = 420;
+constexpr auto MODIFIER_COMPONENT_WIDTH = 420;
+constexpr auto OUTPUT_SETTINGS_COMPONENT_WIDTH = 80;
+constexpr auto EDITOR_WIDTH =
+    GENERAL_SETTINGS_COMPONENT_WIDTH + GENERATOR_COMPONENT_WIDTH +
+    MODIFIER_COMPONENT_WIDTH + OUTPUT_SETTINGS_COMPONENT_WIDTH;
+static_assert(EDITOR_WIDTH == 1040);
 }  // namespace
 
 EdenSynthAudioProcessorEditor::EdenSynthAudioProcessorEditor(
@@ -19,7 +28,7 @@ EdenSynthAudioProcessorEditor::EdenSynthAudioProcessorEditor(
       _generatorComponent(vts, adapter.getPathProvider()),
       _modifierComponent(vts, adapter),
       _outputSettingsComponent(vts) {
-  setSize(1040, EDITOR_HEIGHT);
+  setSize(EDITOR_WIDTH, EDITOR_HEIGHT);
 
   addAndMakeVisible(_generalSettingsComponent);
   addAndMakeVisible(_generatorComponent);
@@ -33,22 +42,21 @@ void EdenSynthAudioProcessorEditor::paint(Graphics& g) {
 }
 
 void EdenSynthAudioProcessorEditor::resized() {
-  _presetsComponent.setBounds(0, 0, getWidth(), PRESETS_COMPONENT_HEIGHT);
+  _presetsComponent.setBounds(0, 0, EDITOR_WIDTH, PRESETS_COMPONENT_HEIGHT);
 
   _generalSettingsComponent.setBounds(
-      0, _presetsComponent.getY() + _presetsComponent.getHeight(), 120,
-      SYNTHESIZER_CONTROLS_COMPONENTS_HEIGHT);
+      0, _presetsComponent.getY() + _presetsComponent.getHeight(),
+      GENERAL_SETTINGS_COMPONENT_WIDTH, SYNTHESIZER_CONTROLS_COMPONENTS_HEIGHT);
   _generatorComponent.setBounds(
       _generalSettingsComponent.getX() + _generalSettingsComponent.getWidth(),
-      _generalSettingsComponent.getY(), 420,
+      _generalSettingsComponent.getY(), GENERATOR_COMPONENT_WIDTH,
       SYNTHESIZER_CONTROLS_COMPONENTS_HEIGHT);
   _modifierComponent.setBounds(
       _generatorComponent.getX() + _generatorComponent.getWidth(),
-      _generalSettingsComponent.getY(), 420,
+      _generalSettingsComponent.getY(), MODIFIER_COMPONENT_WIDTH,
       SYNTHESIZER_CONTROLS_COMPONENTS_HEIGHT);
   _outputSettingsComponent.setBounds(
       _modifierComponent.getX() + _modifierComponent.getWidth(),
-      _modifierComponent.getY(),
-      getWidth() - (_modifierComponent.getX() + _modifierComponent.getWidth()),
+      _modifierComponent.getY(), OUTPUT_SETTINGS_COMPONENT_WIDTH,
       SYNTHESIZER_CONTROLS_COMPONENTS_HEIGHT);
 }
