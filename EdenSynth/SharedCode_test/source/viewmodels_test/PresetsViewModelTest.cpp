3077f054f96a7844ffe7ddcd8535d41c56f906c4
Jan Wilczek <janekw@vp.pl>
2023-05-22 15:30:29 +0200

    SharedCode_test: Added a dependency on EdenSynth.

--- EdenSynth/SharedCode_test/source/viewmodels_test/PresetsViewModelTest.cpp
+++ EdenSynth/SharedCode_test/source/viewmodels_test/PresetsViewModelTest.cpp
@@ -1,7 +1,17 @@
 #include "gtest/gtest.h"
+#include "viewmodels/PresetsViewModel.h"
+#include "PresetManager.h"
 
 namespace eden_vst_test {
+class FakePresetManager : public eden_vst::PresetManager {
+  void saveCurrentPreset(std::function<void(const std::string&)>) override {}
+  void loadPreset(const std::string&) override {}
+  std::vector<std::string> presets() const override { return {}; }
+};
+
 TEST(Foo, Bar) {
+  FakePresetManager manager{};
+  eden_vst::viewmodels::PresetsViewModel viewModel{manager};
   ASSERT_FALSE(true);
 }
 }  // namespace eden_vst_test
