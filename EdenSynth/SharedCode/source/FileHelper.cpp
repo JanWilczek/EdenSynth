5acc1f380852caae2d25bf25349b38ebfa98a82f
Jan Wilczek <24981875+JanWilczek@users.noreply.github.com>
2024-07-01 17:40:30 +0200

    Added Standalone target. Copy assets properly
    Look for assets in the exe path and the parent path.
    
    If it doesn't exist, create one. The same for presets

--- EdenSynth/SharedCode/source/FileHelper.cpp
+++ EdenSynth/SharedCode/source/FileHelper.cpp
@@ -3,16 +3,39 @@
 
 namespace eden_vst {
 std::filesystem::path FileHelper::assetsPath() {
-  return std::filesystem::path(
-             File::getSpecialLocation(
-                 File::SpecialLocationType::currentExecutableFile)
-                 .getParentDirectory()
-                 .getFullPathName()
-                 .toStdString()) /
-         "assets";
+  const auto pluginDirectory = std::filesystem::path(
+      File::getSpecialLocation(File::SpecialLocationType::currentExecutableFile)
+          .getParentDirectory()
+          .getFullPathName()
+          .toStdString());
+  constexpr auto ASSETS_DIRNAME = "assets";
+
+  // Is the assets folder in the folder with the executable?
+  if (const auto assetsDirectory = (pluginDirectory / ASSETS_DIRNAME);
+      std::filesystem::exists(assetsDirectory)) {
+    return assetsDirectory;
+  }
+
+  // Is the assets folder in the parent folder of the executable?
+  if (const auto assetsDirectory =
+          (pluginDirectory.parent_path() / ASSETS_DIRNAME);
+      std::filesystem::exists(assetsDirectory)) {
+    return assetsDirectory;
+  }
+
+  // Create the assets folder.
+  const auto assetsDirectory = (pluginDirectory / ASSETS_DIRNAME);
+  std::filesystem::create_directory(assetsDirectory);
+  return assetsDirectory;
 }
 
 std::filesystem::path FileHelper::presetsPath() {
-  return assetsPath() / "presets";
+  const auto presetsPath = assetsPath() / "presets";
+
+  if (not std::filesystem::exists(presetsPath)) {
+    std::filesystem::create_directory(presetsPath);
+  }
+
+  return presetsPath;
 }
 }  // namespace eden_vst
