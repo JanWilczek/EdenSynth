516f0c9724d5dba48b4188cc864e9cfd9e5c28d7
Jan Wilczek <24981875+JanWilczek@users.noreply.github.com>
2024-07-11 18:43:15 +0200

    Added comments

--- EdenSynth/SharedCode_test/source/ProductionPresetManagerTest.cpp
+++ EdenSynth/SharedCode_test/source/ProductionPresetManagerTest.cpp
@@ -6,6 +6,7 @@
 
 namespace eden_vst_test {
 TEST(ProductionPresetManager, SaveAndLoadPreset) {
+  // given an audio processor with a parameter
   EdenSynthAudioProcessor audioProcessor{};
   auto sampleParameter =
       audioProcessor.getPluginParameters().getRawParameterValue(
@@ -17,8 +18,11 @@ TEST(ProductionPresetManager, SaveAndLoadPreset) {
   constexpr auto presetName = "TestPreset";
   presetManager.saveCurrentPreset(presetName);
   *sampleParameter = 450.f;
+
+  // when we load a preset
   presetManager.loadPreset(presetName);
 
+  // then plugin parameters get overwritten
   ASSERT_FLOAT_EQ(440.f, *sampleParameter);
 }
 }  // namespace eden_vst_test
