335812aa841f30f12df50ddda33394c39169628c
Jan Wilczek <24981875+JanWilczek@users.noreply.github.com>
2025-06-25 10:45:47 +0200

    Created waveshaper curve parameter

--- EdenSynth/SharedCode/source/EdenWaveshapingParameters.cpp
+++ EdenSynth/SharedCode/source/EdenWaveshapingParameters.cpp
@@ -6,6 +6,7 @@
 #include <eden/EdenSynthesiser.h>
 #include "WaveshapingTransferFunctionContainer.h"
 #include "eden/WaveshapingFunctionGenerator.h"
+#include "ParameterIds.h"
 
 namespace eden_vst {
 EdenWaveshapingParameters::EdenWaveshapingParameters(
@@ -24,6 +25,17 @@ void EdenWaveshapingParameters::addWaveshapingParameters(
       NormalisableRange<float>(0.f, 1.f, 1.f), 0.f));
   _autoMakeUpGain =
       valueTreeState.getRawParameterValue(autoMakeUpGainParameterName);
+
+  // TODO: Add
+  // - spread value
+  // - spread seed
+  // - selected curve (AvailableCurves enum)
+  using namespace eden::plugin;
+  valueTreeState.createAndAddParameter(std::make_unique<AudioParameterChoice>(
+      parameter_id::make(parameter_id::WAVESHAPER_CURVE), "Waveshaper curve",
+      StringArray{"Identity", "Hyperbolic tangent", "Chebyshev polynomial"},
+      0));
+  // - Chebyshev polynomial order
 }
 
 void EdenWaveshapingParameters::updateWaveshapingParameters() {
