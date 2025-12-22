4a74ab16d55217335e1b387b176954d090026b2f
Jan Wilczek <24981875+JanWilczek@users.noreply.github.com>
2025-12-22 11:36:22 +0100

    Made the test pass

--- EdenSynth/SharedCode/include/parameters/Parameters.h
+++ EdenSynth/SharedCode/include/parameters/Parameters.h
@@ -4,10 +4,15 @@
 namespace eden::plugin {
 class Parameters {
 public:
-  explicit Parameters(juce::ValueTree) {}
+  explicit Parameters(juce::ValueTree vt) : _valueTree{std::move(vt)} {}
 
-  juce::var toVar() const { return {}; }
+  juce::var toVar() const {
+    DynamicObject::Ptr root{new juce::DynamicObject};
+    root->setProperty("parameters", juce::Array<juce::var>{});
+    return {root};
+  }
 
 private:
+  juce::ValueTree _valueTree;
 };
 }  // namespace eden::plugin
