78f16111298937b61db1e615a881b18d1b56b22d
Jan Wilczek <24981875+JanWilczek@users.noreply.github.com>
2026-04-17 19:58:06 +0200

    test: add serialization test
    It fails!

--- EdenSynth/SharedCode_test/source/PluginProcessorTest.cpp
+++ EdenSynth/SharedCode_test/source/PluginProcessorTest.cpp
@@ -1,11 +1,39 @@
+#include <vector>
+#include <ranges>
 #include <PluginProcessor.h>
 #include <gtest/gtest.h>
 
 namespace eden::plugin {
 TEST(PluginProcessor, SetStateInformationRestoresStateCorrectly) {
-  juce::ScopedJuceInitialiser_GUI _guiInitializer;
-  EdenSynthAudioProcessor audioProcessor{};
-  [[maybe_unused]] auto& parameters = audioProcessor.getPluginParameters();
-  // TODO
+  const juce::ScopedJuceInitialiser_GUI guiInitializer;
+  std::unique_ptr<juce::AudioProcessor> audioProcessor{createPluginFilter()};
+  const auto& parameters = audioProcessor->getParameters();
+
+  // set parameters to random values and save them
+  juce::Random random{42};
+  std::vector<float> parameterValues;
+  parameterValues.reserve(static_cast<size_t>(parameters.size()));
+  for (auto* parameter : parameters) {
+    parameter->setValue(random.nextFloat());
+    parameterValues.push_back(parameter->getValue());
+  }
+
+  // serialize state
+  juce::MemoryBlock serializedState;
+  audioProcessor->getStateInformation(serializedState);
+
+  // create a new plugin instance
+  std::unique_ptr<juce::AudioProcessor> newAudioProcessor{createPluginFilter()};
+
+  // deserialize state
+  newAudioProcessor->setStateInformation(
+      serializedState.getData(), static_cast<int>(serializedState.getSize()));
+
+  // ensure that parameters have equal values
+  ASSERT_EQ(parameters.size(), newAudioProcessor->getParameters().size());
+  for (const auto& [parameter, value] :
+       std::views::zip(newAudioProcessor->getParameters(), parameterValues)) {
+    EXPECT_FLOAT_EQ(parameter->getValue(), value) << parameter->getName(100);
+  }
 }
 }  // namespace eden::plugin
