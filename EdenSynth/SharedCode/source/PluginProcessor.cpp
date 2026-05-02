afdcf9b984ef534166be3f3134ff447e8b4cbb4c
Jan Wilczek <24981875+JanWilczek@users.noreply.github.com>
2026-05-02 13:37:16 +0200

    feat: use serializable DTOs instead of var with custom structure
    migrated plugin parameters to use DTOs instead of wolfsound::SerializedParameters

--- EdenSynth/SharedCode/source/PluginProcessor.cpp
+++ EdenSynth/SharedCode/source/PluginProcessor.cpp
@@ -10,8 +10,104 @@
 #include <wolfsound/juce/wolfsound_SerializedParameters.hpp>
 #include <ranges>
 #include <utility/EdenAssert.h>
+#include "presets/Preset.h"
 
 namespace eden::plugin {
+namespace {
+class UpdatingParameterVisitor : public wolfsound::JuceParameterVisitor {
+  decltype(auto) findParameterWithId(const std::string& id) const noexcept {
+    return std::ranges::find_if(
+        _parameters, [&](const ParameterIdAndValue& p) { return p.id == id; });
+  }
+
+public:
+  UpdatingParameterVisitor(const ParameterValues& parameters)
+      : _parameters{parameters} {}
+
+  void visit(AudioParameterBool& p) override { visitImpl<bool>(p); }
+
+  void visit(AudioParameterFloat& p) override { visitImpl<float>(p); }
+
+  void visit(AudioParameterInt& p) override { visitImpl<int>(p); }
+
+  void visit(AudioParameterChoice& parameter) override {
+    const auto it =
+        findParameterWithId(parameter.getParameterID().toStdString());
+    if (it != _parameters.end()) {
+      if (const auto* const choiceName = get_if<std::string>(&(it->value))) {
+        const auto choiceIndex = parameter.choices.indexOf(*choiceName);
+        // don't "clamp" incorrect values, ignore them
+        if (0 <= choiceIndex && choiceIndex < parameter.choices.size()) {
+          parameter = choiceIndex;
+        }
+      }
+    }
+  }
+
+private:
+  template <typename T>
+  void visitImpl(auto& parameter) const {
+    const auto it =
+        findParameterWithId(parameter.getParameterID().toStdString());
+    if (it != _parameters.end()) {
+      if (const auto value = get_if<T>(&(it->value))) {
+        parameter = *value;
+      }
+    }
+  }
+
+  const ParameterValues& _parameters;
+
+  JUCE_DECLARE_NON_MOVEABLE(UpdatingParameterVisitor)
+};
+
+void update(wolfsound::JuceParameterHolder& ph,
+            const ParameterValues& parameters) {
+  UpdatingParameterVisitor visitor{parameters};
+  ph.accept(visitor);
+}
+
+class ParameterValuesExtractor : public wolfsound::JuceParameterVisitor {
+public:
+  ParameterValuesExtractor() = default;
+
+  void visit(juce::AudioParameterFloat& parameter) override {
+    visitImpl(parameter, parameter.get());
+  }
+
+  void visit(juce::AudioParameterBool& parameter) override {
+    visitImpl(parameter, parameter.get());
+  }
+
+  void visit(juce::AudioParameterInt& parameter) override {
+    visitImpl(parameter, parameter.get());
+  }
+
+  void visit(juce::AudioParameterChoice& parameter) override {
+    visitImpl(parameter, parameter.getCurrentChoiceName().toStdString());
+  }
+
+  [[nodiscard]] ParameterValues result() const { return _result; }
+
+private:
+  template <class P, class V>
+  void visitImpl(const P& parameter, V&& value) {
+    _result.emplace_back(parameter.getParameterID().toStdString(),
+                         std::forward<V>(value));
+  }
+
+  ParameterValues _result;
+
+  JUCE_DECLARE_NON_MOVEABLE(ParameterValuesExtractor)
+};
+
+ParameterValues parameterIdsAndValues(wolfsound::JuceParameterHolder& ph) {
+  ParameterValuesExtractor visitor;
+  ph.accept(visitor);
+  return visitor.result();
+}
+}  // namespace
+
 EdenSynthAudioProcessor::EdenSynthAudioProcessor(
     wolfsound::JuceParameterHolder::Builder parameterBuilder)
     :
@@ -161,11 +257,11 @@ AudioProcessorEditor* EdenSynthAudioProcessor::createEditor() {
 
 //==============================================================================
 void EdenSynthAudioProcessor::getStateInformation(MemoryBlock& destData) {
-  const auto serializedParameters = wolfsound::SerializedParameters::from(
-      wolfsound::toVarArray(_pluginParameters));
+  const auto parameters = parameterIdsAndValues(_pluginParameters);
+  const auto serializedParameters = juce::ToVar::convert(parameters);
   if (serializedParameters.has_value()) {
     juce::MemoryOutputStream memory{destData, true};
-    juce::JSON::writeToStream(memory, serializedParameters->toVar());
+    juce::JSON::writeToStream(memory, serializedParameters.value());
   }
 }
 
@@ -174,10 +270,12 @@ void EdenSynthAudioProcessor::setStateInformation(const void* data,
   juce::MemoryInputStream inputStream{data, static_cast<size_t>(sizeInBytes),
                                       false};
   const auto deserializedParameters = juce::JSON::parse(inputStream);
-  const auto parameters =
-      wolfsound::SerializedParameters::from(deserializedParameters);
+  const auto parameters = juce::FromVar::convert<ParameterValues>(
+      deserializedParameters.hasProperty("parameters")
+          ? deserializedParameters["parameters"]
+          : deserializedParameters);
   if (parameters.has_value()) {
-    wolfsound::update(_pluginParameters, parameters->toVarArray());
+    update(_pluginParameters, parameters.value());
   }
 }
 
